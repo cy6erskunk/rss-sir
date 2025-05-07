7c4303ac794572633358069fd7379f5b2ff8f801
Igor Shevchenko <cyberskunk@gmail.com>
2025-05-07 18:54:39 +0300

    Switch to Admin SDK

---
+++ src/firebase/server.ts
@@ -0,0 +1,28 @@
+import admin from 'firebase-admin';
+import type { ServiceAccount } from 'firebase-admin';
+
+// Construct the credentials object from environment variables
+const serviceAccount = {
+    project_id: import.meta.env.FIREBASE_PROJECT_ID,
+    private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: import.meta.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
+    client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
+    client_id: import.meta.env.FIREBASE_CLIENT_ID,
+    auth_uri: import.meta.env.FIREBASE_AUTH_URI,
+    token_uri: import.meta.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: "googleapis.com"
+};
+
+// Initialize Firebase Admin SDK only if it hasn't been initialized yet
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount as ServiceAccount)
+    });
+}
+
+const db = admin.firestore();
+const auth = admin.auth(); // If you need to interact with Firebase Authentication
+
+export { db, auth, admin }; // Export what you need
